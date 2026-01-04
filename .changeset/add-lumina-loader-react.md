fd4cbfba834d1cc77e7eb8a94796dd3997a1553e
Ehsan <ehsunzar@yahoo.com>
2026-01-04 13:18:58 +0330

    feat(release): scaffold unscoped adapter packages (react, vue) and set changesets to public

---
+++ .changeset/add-lumina-loader-react.md
@@ -0,0 +1,5 @@
+---
+'lumina-loader-react': patch
+---
+
+Add initial `lumina-loader-react` adapter package (initial patch release).
---
+++ packages/vue/src/index.ts
@@ -0,0 +1,2 @@
+export { default as LuminaLoaderVue } from '../../../src/adapters/vue/LuminaLoader.vue';
+export * from '../../../src/adapters/vue/LuminaLoader.vue';
